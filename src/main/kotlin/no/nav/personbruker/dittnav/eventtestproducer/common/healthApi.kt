1919905993e8734848b7637e771d8b335029bc99
Robert Larsen <robert.larsen@nav.no>
2020-09-14 09:22:22 +0200

    Retter midlertidig innført feil

--- src/main/kotlin/no/nav/personbruker/dittnav/eventtestproducer/common/healthApi.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventtestproducer/common/healthApi.kt
@@ -13,7 +13,7 @@ fun Routing.healthApi(collectorRegistry: CollectorRegistry = CollectorRegistry.d
 
     val pingJsonResponse = """{"ping": "pong"}"""
 
-    get("/isAlivee") {
+    get("/isAlive") {
         call.respondText(text = "ALIVE", contentType = ContentType.Text.Plain)
     }
 
