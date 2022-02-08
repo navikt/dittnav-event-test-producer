6971fbb1110d8bdade231717ec50719c56db30ca
Robert Larsen <robert.larsen@nav.no>
2022-02-08 15:06:44 +0100

    Endrer fra post til get midlertidig for done/all

--- src/main/kotlin/no/nav/personbruker/dittnav/eventtestproducer/done/doneApi.kt
+++ src/main/kotlin/no/nav/personbruker/dittnav/eventtestproducer/done/doneApi.kt
@@ -1,13 +1,12 @@
 package no.nav.personbruker.dittnav.eventtestproducer.done
 
-import io.ktor.routing.Route
-import io.ktor.routing.post
+import io.ktor.routing.*
 import no.nav.personbruker.dittnav.eventtestproducer.config.innloggetBruker
 import no.nav.personbruker.dittnav.eventtestproducer.config.respond
 
 fun Route.doneApi(doneEventService: DoneEventService) {
 
-    post("/produce/done/all") {
+    get("/produce/done/all") {
         respond {
             doneEventService.markAllBrukernotifikasjonerAsDone(innloggetBruker)
             "Done-eventer er produsert for alle for brukeren: $innloggetBruker sine brukernotifikasjoner."
